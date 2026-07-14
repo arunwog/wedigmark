c236340c7c272c4a1441933dc349ca74ad4a43e6
Amogh <arunvasishta1211@gmail.com>
2026-07-14 22:29:18 +0530

    Make logo link to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import Footer from "@/components/sections/Footer";
 export default function Home() {
      return (
           <>
-               <Navbar />
+
                <Hero />
                <Services />
                <Process />
--- components/sections/Navbar.tsx
+++ components/sections/Navbar.tsx
@@ -1,4 +1,5 @@
 import Button from "@/components/ui/Button";
+import Link from "next/link";
 
 export default function Navbar() {
     return (
@@ -6,10 +7,21 @@ export default function Navbar() {
             <div className="mx-auto flex max-w-7xl items-center justify-between px-8 py-5">
 
                 <h1 className="text-2xl font-bold text-white">
-                    We<span className="text-orange-500">Dig</span>Mark
+
+                    <Link href="/" className="text-2xl font-bold text-white">
+                        We<span className="text-orange-500">Dig</span>Mark
+                    </Link>
                 </h1>
 
                 <div className="hidden md:flex items-center gap-8 text-gray-300">
+
+                    <Link
+                        href="/"
+                        className="transition hover:text-orange-500"
+                    >
+                        Home
+                    </Link>
+
                     <a
                         href="/services"
                         className="transition hover:text-orange-500"
