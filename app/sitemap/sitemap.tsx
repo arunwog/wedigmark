3301584677171aeb41068562b22440a6e8bd9ee1
LocalHost134 <harshady130408@gmail.com>
2026-08-11 00:20:33 +0530

    Might work bro

--- app/sitemap/sitemap.tsx
+++ app/sitemap/sitemap.tsx
@@ -1,3 +1,5 @@
+import type { MetadataRoute } from "next"
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     { url: 'https://wedigmark.online', lastModified: new Date() },
